8d9ce4c51c5151378d2a28b02ac11ad0febccfe6
Gabriel "thebe111" Fonseca <gabriel.fonseca.xyz@gmail.com.br>
2023-06-05 23:00:13 -0300

    feat: initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,193 +1,144 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import * as React from "react";
+import type { HeadFC, PageProps } from "gatsby";
+import { 
+    CompanyHeader,
+    CompanyHeaderDot,
+    CompanyHeaderEffect,
+    CompanyName,
+    CompanySection,
+    ContactSection,
+    Footer,
+    FooterCompanyName,
+    FooterParagraph,
+    MailLink,
+    Main,
+    MainParagraph,
+    PortfolioDescription,
+    PortfolioSection,
+    SectionHeader,
+    PortfolioDescriptionHeader,
+    PortfolioDescriptionDetails,
+    PortfolioContent,
+    WorkSection,
+    WorkItemNumber,
+    WorkItemHeader,
+    WorkItem,
+    ItemParagraph,
+    ItemA,
+    ItemB,
+    ItemC,
+    ItemD,
+    FooterSectionHeader,
+} from "../styles/index";
+import { StaticImage } from "gatsby-plugin-image";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+const portfolio = [
+    {
+        id: 1,
+        company: "SoluCX",
+        uri: "https://solucx.com.br/",
+        description: "We give voice to more than 30 million customers annually. Through a complete ecosystem of Customer Experience solutions, we offer education, consulting and technology to generate insights and listen to customers, in addition to recognizing professionals and companies that stand out in the customer-centric era",
+    },
+];
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-const IndexPage: React.FC<PageProps> = () => {
+const HomePage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+      <>
+          <Main>
+              <CompanySection>
+                  <CompanyName>
+                      <CompanyHeader>
+                          Alpha<CompanyHeaderDot>.</CompanyHeaderDot>Digital
+                      </CompanyHeader>
+                      <CompanyHeaderEffect></CompanyHeaderEffect>
+                  </CompanyName>
+
+                  <MainParagraph>
+                      Trabalhamos os detalhes provendo as melhores soluções em qualidade, performance, e manutenibilidade.
+                  </MainParagraph>
+              </CompanySection>
+
+              <PortfolioSection>
+                  <SectionHeader>Portfolio</SectionHeader>
+
+                  <PortfolioContent>
+                      <StaticImage 
+                          src="../../static/assets/solucx_presentation_image.png" 
+                          alt="Company representative image"
+                          className="img"
+                          layout="constrained"
+                          transformOptions={{ fit: "contain" }}
+                      />
+                      <PortfolioDescription>
+                          <PortfolioDescriptionHeader>SoluCX</PortfolioDescriptionHeader>
+                          <PortfolioDescriptionDetails>
+                              Trabalhos juntos no serviço core de disparos de mensagens onde conseguimos aumentar a performance e reduzir em 00% os custos. Conseguimos servir mais clientes e trabalhar de forma a facilitar a manutenção e eventual integrações ao projeto
+                          </PortfolioDescriptionDetails>
+                      </PortfolioDescription>
+                  </PortfolioContent>
+              </PortfolioSection>
+
+              <SectionHeader>Como Trabalhamos?</SectionHeader>
+
+              <WorkSection>
+                  <ItemA>
+                      <WorkItem>
+                          <WorkItemNumber>1</WorkItemNumber>
+                          <WorkItemHeader>Mapeamento</WorkItemHeader>
+                      </WorkItem>
+
+                      <ItemParagraph>
+                          Entendemos seu negócio de ponta a ponta, os requisitos, as regras do negócio os critérios de aceitação e os objetivos do projeto e definiremos o que será mais urgente no seu contexto, trabalhando uma solução sob medida
+                      </ItemParagraph>
+                  </ItemA>
+
+                  <ItemB>
+                      <WorkItem>
+                          <WorkItemNumber>2</WorkItemNumber>
+                          <WorkItemHeader>Planejamento</WorkItemHeader>
+                      </WorkItem>
+
+                      <ItemParagraph>
+                          Passamos a separar as melhores metodologias e tecnologias transformando essa informações em um calendário ágil evitando possíveis dificuldades e impedimentos
+                      </ItemParagraph>
+                  </ItemB>
+
+                  <ItemC>
+                      <WorkItem>
+                          <WorkItemNumber>3</WorkItemNumber>
+                          <WorkItemHeader>Desenvolvimento</WorkItemHeader>
+                      </WorkItem>
+
+                      <ItemParagraph>
+                          Aqui nossos especialistas entram em ação, trabalhamos com metodologias ágeis e com isso as entregas acontecem de forma constante a cada duas semanas. Demonstramos a evolução do projeto, alinhando os objetivos e tudo isso com uma comunicação objetiva e constante com você parceiro. Todo o processo é focado em entrega de valor com um desenvolvimento baseado em testes
+                      </ItemParagraph>
+                  </ItemC>
+
+                  <ItemD>
+                      <WorkItem>
+                          <WorkItemNumber>4</WorkItemNumber>
+                          <WorkItemHeader>Suporte</WorkItemHeader>
+                      </WorkItem>
+
+                      <ItemParagraph>
+                          Realizamos desde do dia zero toda a documentação do projeto para que você parceiro possa ser independente do nosso time, e ficamos disponíveis a suporte para garantir os objetivos e metas definidos para o projeto com uma garantia de 60 dias
+                      </ItemParagraph>
+                  </ItemD>
+              </WorkSection>
+          </Main>
+
+          <Footer>
+              <ContactSection>
+                  <FooterSectionHeader>Entre em contato</FooterSectionHeader>
+                  <MailLink href="mailto:contact@adigital.co">contact@adigital.co</MailLink>
+              </ContactSection>
+
+              <FooterParagraph>Created by <FooterCompanyName>@alphadigital.</FooterCompanyName> | Street Frederico Peixoto, 123 - Sala 2. Vitória da Conquista - BA</FooterParagraph>
+          </Footer>
+      </>
   )
 }
 
-export default IndexPage
+export default HomePage;
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Alpha.Digital</title>
